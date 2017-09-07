5774ca7d7f38e9d66ad4dd1e7b5c6c6b2d9854d9
Roberto Antelo <roberto_antelo@hotmail.com>
2017-09-06 22:28:08 -0500

    Fix broken class names

--- contentscript.js
+++ contentscript.js
@@ -1,18 +1,18 @@
 //Script to be executed in the actual browser page
 
-var formatTimestamps = arr_of_timestamps => arr_of_timestamps.map(e => e.innerText);
+var formatTimestamps = arr_of_timestamps => arr_of_timestamps.map(e => e.outerText);
 var formatOwners = arr_of_owners => arr_of_owners.map(e => e.outerText);
 
 var init = () => {
-  let dom_timestamps = Array.from(document.getElementsByClassName("timestamp"));
+  let dom_timestamps = Array.from(document.getElementsByClassName("ytd-thumbnail-overlay-time-status-renderer"));
   let raw_timestamps = formatTimestamps(dom_timestamps);
   let dom_owners = Array.from(document.getElementsByClassName("pl-video-owner"));
   let raw_owners = formatOwners(dom_owners);
-  let pl_title = document.getElementsByClassName("pl-header-title")[0].innerText;
-  let pl_header = document.getElementsByClassName("pl-header-thumb")[0];
+  let pl_title = document.getElementById("title").innerText;
+  let pl_header = document.getElementById("playlist-thumbnails");
   let thumb_src = pl_header.getElementsByTagName("img")[0].getAttribute("src");
-  let play_link = pl_header.getElementsByTagName("a")[0].getAttribute("href");
-  let owner = document.getElementsByClassName("qualified-channel-title-text")[0].innerText;
+  let play_link = document.getElementById("thumbnail").getAttribute("href");
+  let owner = document.getElementById("owner-name").innerText;
 
   chrome.runtime.sendMessage({
     timestamps: raw_timestamps,
